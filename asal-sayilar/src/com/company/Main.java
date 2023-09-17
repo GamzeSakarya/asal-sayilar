9fc26e6e4839061a67c2a61e51b356ff6c6cbdf5
Gamze Sakarya <126356427+GamzeSakarya@users.noreply.github.com>
2023-09-17 14:56:27 +0300

    Add files via upload

---
+++ asal-sayilar/src/com/company/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Scanner input = new Scanner(System.in);
+
+	System.out.print("Üst Sınırı Giriniz :");
+	int ustSinir = input.nextInt();
+
+	System.out.println("1 ile " + ustSinir +" arasındaki asal sayılar :");
+	for (int i = 2; i <= ustSinir; i++) {
+	    boolean asal = true;
+	    for (int j = 2; j < i; j++){
+	        if (i % j == 0){
+	            asal = false;
+	            break;
+            }
+        }
+	    if (asal) {
+	        System.out.print(i + " ");
+
+        }
+    }
+    }
+}
